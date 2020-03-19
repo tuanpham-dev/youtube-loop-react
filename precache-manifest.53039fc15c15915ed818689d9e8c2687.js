aab429b1cc7f5c3efabca5d45197b369730fcddd
Tuan Pham <tuanpham.developer@gmail.com>
2020-03-19 17:25:34 -0400

    Updates

---
+++ precache-manifest.53039fc15c15915ed818689d9e8c2687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1223dd5a9632794820e2a0734ff3fc",
+    "url": "/youtube-loop-react/index.html"
+  },
+  {
+    "revision": "759fabc4714e540c6df2",
+    "url": "/youtube-loop-react/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "759fabc4714e540c6df2",
+    "url": "/youtube-loop-react/static/js/2.64f0e1b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-loop-react/static/js/2.64f0e1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f0add11c2ceabb31da",
+    "url": "/youtube-loop-react/static/js/main.270e1329.chunk.js"
+  },
+  {
+    "revision": "76009a9c4edb6818d7eb",
+    "url": "/youtube-loop-react/static/js/runtime-main.88966c61.js"
+  }
+]);
